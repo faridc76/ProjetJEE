0f995b1e51a69a43344e461b52ce402eba5f40b5
faridc76 <farid.chouakria@live.fr>
2015-03-27 20:01:29 +0100

    Revert "Revert "ProjetJEE""
    This reverts commit e612b49bbd84c3d3f3cd7bafcf3f6d109ee3a907.

---
+++ WebContent/utils/footer.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+	<!-- Footer -->
+    <footer>
+      <div class="container">
+        <hr>
+        <div class="row">
+          <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 text-center">
+            <p id="copyright">ESIGELEC - Java/JEE programming (GSI204-A)</p>
+          </div>
+          <div class="col-lg-3 col-lg-offset-5 col-md-4 col-md-offset-3 col-sm-5 col-sm-offset-1 col-xs-12 text-center">
+            <p class=""><a href="http://fr.linkedin.com/pub/mincong-huang/a6/737/148/">Alexandre Deneuve</a> | <a href="http://fchouakria.free.fr">Farid CHOUAKRIA</p>
+          </div>
+        </div><!-- /.row -->
+      </div>
+    </footer>
+</div>
+</body>
+</html>
---
+++ src/controller/RequestLoginServlet.java
@@ -0,0 +1,62 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Utilisateur;
+import model.db.UtilisateurDB;
+import model.db.exception.DatabaseAccessError;
+
+@WebServlet("/RequestSignUpServlet")
+public class RequestLoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 3311297485258766639L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		PrintWriter out = resp.getWriter();
+		HttpSession session = req.getSession(true);
+		String login = req.getParameter("login");
+		String password = req.getParameter("password");
+		
+		boolean loginOk = false;
+		try {
+			loginOk = UtilisateurDB.checkLogin(login, password);
+		} catch (DatabaseAccessError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (loginOk) {
+			Utilisateur us = null;
+			try {
+				us = UtilisateurDB.getUtilisateur(login);
+			} catch (DatabaseAccessError e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// create session
+			
+			session.setAttribute("login", login);
+			session.setAttribute("name", us.getPrenom() + " " + us.getNom());
+			RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+			dispatcher.forward(req, resp);
+		} else {
+			out.println("nok");
+			RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+			dispatcher.forward(req, resp);
+		}
+		
+	}
+
+}
